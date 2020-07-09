c2f42be5ed505c21b42277a4e1965930f8ae978e
zcmimi <36293036+zcmimi@users.noreply.github.com>
2020-07-10 00:00:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'mirror.zcmimi.vercel.app',
+    serverName: 'mirror-zcmimi.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
